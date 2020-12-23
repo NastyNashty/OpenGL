37977d64c5ed8e0851332c9ae632713752d6b6e3
LoisNgm <lois.ngm@digipen.edu>
2020-12-23 13:02:53 +0800

    Added comments

--- OpenGL/OpenGL/main.cpp
+++ OpenGL/OpenGL/main.cpp
@@ -15,12 +15,17 @@ void processInput(GLFWwindow* window)
 
 int main()
 {
+  // BEGIN - Initialize GLFW 
   glfwInit();
+  // Specify OpenGL version 3.3 (Major = 3.0, Minor = 0.3)
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  // Add next line if using MAC OS X
   //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
+  // END - Initialize GLFW
+  
+  // BEGIN - Create window object
   GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
   if (window == NULL)
   {
@@ -29,29 +34,41 @@ int main()
     return -1;
   }
   glfwMakeContextCurrent(window);
+  // END - Create window object
 
+  // BEGIN - Initialize GLAD for OpenGL operations
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
   {
     std::cout << "Failed to initialize GLAD" << std::endl;
     return -1;
   }
+  // END - Initialize GLAD for OpenGL operations
 
+  // Inform dimensions to OpenGL
   glViewport(0, 0, 800, 600);
 
+  // for resizing
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+  // BEGIN - Keep running the window
   while (!glfwWindowShouldClose(window))
   {
+    // Check if ESC is pressed
     processInput(window);
 
+    // BEGIN - Render
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+    // END - Render
 
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
+  // END - Keep running the window
 
+  // BEGIN - Clean up
   glfwTerminate();
+  // END - Clean up
 
   return 0;
 }
